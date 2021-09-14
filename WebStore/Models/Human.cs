07857dde479b212dd485e7e3d86446bd6dc94f44
Александр Izumroot <izumroot@deeplay.io>
2021-09-14 13:02:15 +0600

    done lesson1 tasks

---
+++ WebStore/Models/Human.cs
@@ -0,0 +1,12 @@
+﻿namespace WebStore.Models
+{
+    public class Human
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public byte Age { get; set; }
+    }
+
+}
